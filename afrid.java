369423ed028a60967ea0c68123b9c64f98c36cdd
AFRID <afrid@AFRIDs-MacBook-Air.local>
2026-08-06 21:11:38 +0530

    initial commit

--- afrid.java
+++
@@ -1,5 +0,0 @@
-public class afrid{
-    public static void main(String[] args) {
-        System.out.println("Hello Afrid");
-    }
-}
--- hello.java
+++ hello.java
@@ -1,12 +1,24 @@
-class hello
-{
+import java.util.*;
+
+
+
+public class hello {
     public static void main(String[] args) {
-   int nums[] = {2,3,5,7,10};
-   int target = 7;
-   for (int i = 0;i < nums.length ; i++){
-    if(nums[i] == target){
-        System.out.println("Element found at index "+i);
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<Integer> salaries = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            salaries.add(sc.nextInt());
+        }
+
+        salaries.stream()
+                .map(salary -> salary + (salary * 10 / 100))
+                .forEach(salary -> System.out.print(salary + " "));
+
+        sc.close();
     }
-    }
-}
 }
